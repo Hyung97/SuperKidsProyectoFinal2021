c551bc4958a6f4ed2cd062080ba38aed18b09858
Eduardo Isaías Villegas Ortiz <44987358+Eddo091@users.noreply.github.com>
2021-06-14 21:28:10 -0600

    Chat_Commit

---
+++ resources/views/chat/index.php
@@ -0,0 +1,26 @@
+  <html_entity_decode>
+  <script src="jquery.min.js"></script>
+  <script src="chat.js"></script>
+  <link href="chat.css" rel="stylesheet"/>
+ </head>
+ 
+ <body>
+	<div id="konten">
+	<div class="tabs"><div class="tab" data-dip="chat">SuperKids Chat</div><div class="tab" data-dip="users">Super Usuarios</div></div>
+   <div class="chat">
+     <?php 
+	 include("config.php");include("login.php");
+     if(isset($_SESSION['user'])){
+      include("chatbox.php");
+     }else{
+      $display_case=true;
+      include("login.php");
+     }
+     ?>
+   </div>
+   <div class="users" style='display:none'>
+     <?php include("users.php");?>
+    </div>
+	</div>
+ </body>
+</html_entity_decode>
--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -37,7 +37,7 @@
         </a>
       </li>
       <li class="nav-item{{ $activePage == 'notifications' ? ' active' : '' }}">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{ route('chat.index') }}">
           <i class="material-icons">duo</i>
           <p>{{ __('Chat') }}</p>
         </a>
