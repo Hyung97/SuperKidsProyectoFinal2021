91537e4b15bf761e2bbf02c3c9dd00e325c6a16a
Hyung97 <tammyhyunglee@gmail.com>
2021-06-15 17:51:23 -0600

    mas cambios

--- resources/views/chat/config.php
+++ resources/views/chat/config.php
@@ -5,7 +5,7 @@ if(!isset($dbh)){
  $musername = "root";
  $mpassword = "";
  $hostname  = "localhost";
- $dbname    = "chat_tusolutionweb";
+ $dbname    = "chat";
  $dbh=new PDO('mysql:dbname='.$dbname.';host='.$hostname.";port=3306",$musername, $mpassword);
  /*Change The Credentials to connect to database.*/
  include("user_online.php");
--- resources/views/downloads/download.blade.php
+++ resources/views/downloads/download.blade.php
@@ -2,21 +2,44 @@
 @section('content')
 <div class="content">
 
-<div class="row">
-  <div class="col-sm-12">
-    <div class="card">
-      <div class="card-body">
-      <a href="https://drive.google.com/uc?export=download&id=1JpLDy2-xgXSWTABv7sTqZz0rAqdTrk92" download="Coronafake.pdf">Descargar: Coronafake pdf</a>
-      <p></p>
-      <a href="https://drive.google.com/uc?export=download&id=1c-oC4nC9FazIaYXe-1ApruHQlDAK5vO-" download="Influencia.pdf">Descargar: Influencia Estacional pdf </a>
-      <p></p>
-      <a href="https://drive.google.com/uc?export=download&id=1jNzTzhtciXvMz1iXov5w21ZzkbZYQcKd" download="Comolavarselasmanos.pdf">Descargar: Como lavarse las manos pdf </a>
-      <p></p>
-      <a href="https://drive.google.com/uc?export=download&id=14YDNkS7iJSKtpNW-ayHtum09sCzu28CR" download="NuevoCoronavirus.pdf">Descargar: Informacion sobre el nuevo coronavirus pdf </a>
-      <p></p>
-      <a href="https://drive.google.com/uc?export=download&id=1xmYnP3XjUcfBCoJqGz_Xe2V2UTU6rKUj" download="NuevoModalidad.pdf">Descargar: La nueva modalidad pdf </a>
-      <p></p> 
-      </div>
+
+<div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Coronafake</h5>
+      <a href="https://drive.google.com/uc?export=download&id=1JpLDy2-xgXSWTABv7sTqZz0rAqdTrk92" download="Coronafake.pdf" class="btn btn-info">Descargar</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Influencia Estacional</h5>
+      <a href="https://drive.google.com/uc?export=download&id=1c-oC4nC9FazIaYXe-1ApruHQlDAK5vO-" download="Influencia.pdf" class="btn btn-info">Descargar</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Como lavarse las manos</h5>
+      <a href="https://drive.google.com/uc?export=download&id=1jNzTzhtciXvMz1iXov5w21ZzkbZYQcKd" download="Comolavarselasmanos.pdf" class="btn btn-info">Descargar</a>
+    </div>
+  </div>
+</div>
+<div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Información sobre el nuevo coronavirus</h5>
+      <a href="https://drive.google.com/uc?export=download&id=14YDNkS7iJSKtpNW-ayHtum09sCzu28CR" download="NuevoCoronavirus.pdf" class="btn btn-info">Descargar</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">La nueva modalidad</h5>
+      <a href="https://drive.google.com/uc?export=download&id=1xmYnP3XjUcfBCoJqGz_Xe2V2UTU6rKUj" download="NuevoModalidad.pdf" class="btn btn-info">Descargar</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Yo en casa</h5>
+      <a href="https://drive.google.com/uc?export=download&id=1QmPHlwtNwnYpbx11yeD-nUQRqzKtFZD-" download="Sigamos.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
 </div>
