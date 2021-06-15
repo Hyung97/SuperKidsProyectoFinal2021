43ca652ae349b815c8959e302e56a0be2329d46b
Hyung97 <tammyhyunglee@gmail.com>
2021-06-15 17:54:14 -0600

    cambios en descargas

--- resources/views/downloads/download.blade.php
+++ resources/views/downloads/download.blade.php
@@ -6,19 +6,19 @@
 <div class="card-group">
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Coronafake</h5>
+      <h5 class="card-title">Coronafake.</h5>
       <a href="https://drive.google.com/uc?export=download&id=1JpLDy2-xgXSWTABv7sTqZz0rAqdTrk92" download="Coronafake.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Influencia Estacional</h5>
+      <h5 class="card-title">Influencia Estacional.</h5>
       <a href="https://drive.google.com/uc?export=download&id=1c-oC4nC9FazIaYXe-1ApruHQlDAK5vO-" download="Influencia.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Como lavarse las manos</h5>
+      <h5 class="card-title">Como lavarse las manos.</h5>
       <a href="https://drive.google.com/uc?export=download&id=1jNzTzhtciXvMz1iXov5w21ZzkbZYQcKd" download="Comolavarselasmanos.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
@@ -26,19 +26,19 @@
 <div class="card-group">
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Información sobre el nuevo coronavirus</h5>
+      <h5 class="card-title">Información sobre el nuevo coronavirus.</h5>
       <a href="https://drive.google.com/uc?export=download&id=14YDNkS7iJSKtpNW-ayHtum09sCzu28CR" download="NuevoCoronavirus.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">La nueva modalidad</h5>
+      <h5 class="card-title">La nueva modalidad.</h5>
       <a href="https://drive.google.com/uc?export=download&id=1xmYnP3XjUcfBCoJqGz_Xe2V2UTU6rKUj" download="NuevoModalidad.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Yo en casa</h5>
+      <h5 class="card-title">Yo en casa.</h5>
       <a href="https://drive.google.com/uc?export=download&id=1QmPHlwtNwnYpbx11yeD-nUQRqzKtFZD-" download="Sigamos.pdf" class="btn btn-info">Descargar</a>
     </div>
   </div>
