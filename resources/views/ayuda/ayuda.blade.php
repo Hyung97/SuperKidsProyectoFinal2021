76adb2887868c57f0107b114d6f0274b19f81c51
Hyung97 <tammyhyunglee@gmail.com>
2021-06-15 22:09:54 -0600

    help

--- resources/views/ayuda/ayuda.blade.php
+++ resources/views/ayuda/ayuda.blade.php
@@ -1,38 +1,41 @@
 @extends('layouts.main', ['activePage' => 'ayuda', 'titlePage' => 'Ayuda'])
 @section('content')
 <div class="content">
-<div class="row">
-<div class="card border-dark mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body text-dark">
-    <h5 class="card-title">Dark card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+<div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">¿Qué es SuperKids? </h5>
+      <a href="#" class="btn btn-info">Ver más</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">¿Qué puedo hacer aquí?</h5>
+      <a href="#" class="btn btn-info">Ver más</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">¿Cómo descargo los juegos?</h5>
+      <a href="#" class="btn btn-info">Ver más</a>
+    </div>
   </div>
 </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
+<div class="card-group">
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">¿Con qué navegadores es compatible SuperKids?</h5>
+      <a href="#" class="btn btn-info">Ver más</a>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">No puedo ingresar a los juegos.</h5>
+      <a href="#" class="btn btn-info">Ver más</a>
     </div>
   </div>
 </div>
 
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
 </div>
 @endsection
